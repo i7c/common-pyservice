43b7336ec6afbce5bdeb8f008a2f38aff26a12ed
Constantin Michael Weisser <i7c@posteo.de>
2023-08-15 22:08:34 -0300

    Error messages during setup

--- common/pys/flask_setup.py
+++ common/pys/flask_setup.py
@@ -1,8 +1,19 @@
 from .authentication import AuthInterceptor
 from flask_cors import CORS
+import os
+import sys
+
+
+def fail(message):
+    print(message)
+    sys.exit(2)
 
 
 def setup(app):
+    if not os.getenv('JWKS_ENDPOINT'):
+        fail("You must set env var JWKS_ENDPOINT")
+    if not os.getenv('JWT_AUDIENCE'):
+        fail("You must set env var JWT_AUDIENCE")
     CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
     AuthInterceptor().init_app(app)
