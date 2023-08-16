4405caa5ff02b684ae4ccb5d703b9eb2140ffc3d
Constantin Michael Weisser <i7c@posteo.de>
2023-08-16 20:00:08 -0300

    Dont check for env vars in test setup

--- common/pys/test/test_setup.py
+++ common/pys/test/test_setup.py
@@ -1,19 +1,8 @@
 from .test_authentication import AuthInterceptor
 from flask_cors import CORS
-import os
-import sys
-
-
-def fail(message):
-    print(message)
-    sys.exit(2)
 
 
 def fake_setup(app):
-    if not os.getenv('JWKS_ENDPOINT'):
-        fail("You must set env var JWKS_ENDPOINT")
-    if not os.getenv('JWT_AUDIENCE'):
-        fail("You must set env var JWT_AUDIENCE")
     CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
     AuthInterceptor().init_app(app)
