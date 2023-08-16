b10e860cc4d5a266c91e5989700d42f4bf856cb4
Constantin Michael Weisser <i7c@posteo.de>
2023-08-16 20:22:50 -0300

    Test execution helper

---
+++ common/pys/test/test_exec.py
@@ -0,0 +1,11 @@
+def do_request(hf, rq={}, headers={}):
+    default_headers = {"authorization": "Bearer validtoken",
+                       "x-forwarded-for": "1.2.3.4, 189.110.31.110, 3.2.3.2",
+                       "x-forwarded-proto": "http"}
+    eff_headers = {**default_headers, **headers}
+    default_rq = {"httpMethod": "GET",
+                  "path": "/",
+                  "queryStringParameters": {},
+                  "headers": eff_headers}
+    eff_rq = {**default_rq, **rq}
+    return hf(eff_rq, None)['body']
