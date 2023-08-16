4fd57a45ae19bffc5e5b9f5a81838d614f504a33
Constantin Michael Weisser <i7c@posteo.de>
2023-08-16 19:55:54 -0300

    Add test authentication

---
+++ common/pys/test/test_authentication.py
@@ -0,0 +1,25 @@
+import re
+from flask import abort, g, request
+
+
+class AuthInterceptor(object):
+    auth_header_payload = re.compile("Bearer[\s]+")
+
+    def init_app(self, app):
+        def interceptor():
+            try:
+                if request.method == "OPTIONS":
+                    return
+                auth_header = request.headers.get('authorization')
+                if not auth_header or auth_header.isspace():
+                    abort(401)
+                if not auth_header.startswith('Bearer'):
+                    abort(401)
+                payload = AuthInterceptor.auth_header_payload.sub("", auth_header)
+                if not payload == "validtoken":
+                    abort(401)
+                g.auth_identity = {"sub": "1234"}
+            except Exception:
+                abort(401)
+
+        app.before_request(interceptor)
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='common_pyservice',
-    version='2023.08.15',
+    version='2023.08.16',
     url='https://github.com/michael-D-Fly/common-pyservice',
     author='Michael Weisser',
     author_email='michael@decentrafly.com',
     description='Common code for python microservices',
-    packages=find_packages(),    
+    packages=find_packages(),
     install_requires=[
         'cryptography==41.0.1',
         'flask-cors==4.0.0',
