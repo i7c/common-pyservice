0f5b15a786009e26c42d41fd6303bc1f0e0fb9b7
Constantin Michael Weisser <i7c@posteo.de>
2024-03-16 12:28:33 -0300

    Better Logger Diplomat

--- common/pys/diplomats/log.py
+++ common/pys/diplomats/log.py
@@ -4,14 +4,27 @@ import os
 import sys
 
 
-class LoggerDiplomat(object):
-    def __init__(self):
-        json_logging.init_non_web(enable_json=True)
+initialized = False
+
+
+def init():
+    global initialized
+
+    if not initialized:
         logger = logging.getLogger("system")
         logger.setLevel(os.getenv('LOGLEVEL', 'INFO'))
         logger.addHandler(logging.StreamHandler(sys.stdout))
         logger.debug("common-pys logger initialized")
+        initialized = True
+
+
+class LoggerDiplomat(object):
+    def __init__(self, static_data={}):
+        json_logging.init_non_web(enable_json=True)
+        init()
+        logger = logging.getLogger("system")
         self.logger = logger
+        self.static_data = static_data
 
     def log(self, msg, level=logging.INFO, data=None):
         extra = None
@@ -19,14 +32,14 @@ class LoggerDiplomat(object):
             extra = {"props": {"data": data}}
         self.logger.log(level=level, msg=msg, extra=extra)
 
-    def debug(self, msg, data=None):
-        self.log(msg, level=logging.DEBUG, data=data)
+    def debug(self, msg, data={}):
+        self.log(msg, level=logging.DEBUG, data={**self.static_data, **data})
 
-    def error(self, msg, data=None):
-        self.log(msg, level=logging.ERROR, data=data)
+    def error(self, msg, data={}):
+        self.log(msg, level=logging.ERROR, data={**self.static_data, **data})
 
-    def info(self, msg, data=None):
-        self.log(msg, level=logging.INFO, data=data)
+    def info(self, msg, data={}):
+        self.log(msg, level=logging.INFO, data={**self.static_data, **data})
 
-    def warn(self, msg, data=None):
-        self.log(msg, level=logging.WARN, data=data)
+    def warn(self, msg, data={}):
+        self.log(msg, level=logging.WARN, data={**self.static_data, **data})
